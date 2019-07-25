9064663030eab9fa04f6b65f34e2afbc2b68c152
欢儿虫 <2667833068@qq.com>
2019-07-25 14:34:17 +0800

    更新

--- kits.js
+++ kits.js
@@ -96,3 +96,68 @@ for (var i = 0; i < arr.length; i++) {
   sum += arr[i];
 }
 console.log(sum);
+
+
+var kits = {};
+
+
+// 从浏览器的地址栏里面获取id的封装函数
+kits.getUrlParams = function () {
+  let arr = location.search.substring(1).split('&');
+  let prams = {};
+  arr.forEach(e=>{
+      let temp = e.split('=');
+      let key = temp[0];
+      let val = temp[1];
+      prams[key] = val;
+  })
+  return prams;
+}
+
+
+
+//状态模式的封装
+let strategies = {
+  isNonEmpty: function (val, msg) {
+    if (val.trim().length === 0) {
+      return msg;
+    }
+  },
+  minLength: function (val, len, msg) {
+    if (val.trim().length < len) {
+      return msg;
+    }
+  }
+}
+
+// 状态模式的思想： 使用状态代替if-else
+function Validator() {
+  // 有一个数组，用来存储所有的验证的函数
+  this.validateFuncs = [];
+}
+// 2.2 给构造函数的原型添加一个方法，让其可以添加一个新的函数进去
+Validator.prototype.add = function (dom, arr) {
+  // 遍历数组，往this.validateFuncs 添加新的验证的方法
+  for (let i = 0; i < arr.length; i++) {
+    let fn = function () {
+      let rule = arr[i];
+      let params = rule.fnName.split(':');// [minLength,8]
+      let fnName = params.shift(); // fnName里面可能会包含参数   删除arr中的第一个数
+      params.unshift(dom.value); // [dom.vlaue,8]               添加到arr中的第一个
+      params.push(rule.errMsg); // [dom.value,8,rule.errMsg];
+      return strategies[fnName].apply(dom, params);
+    }
+    this.validateFuncs.push(fn);
+  }
+
+}
+
+// 2.3 需要一个可以把数组里面的每个函数都执行的方法
+Validator.prototype.start = function () {
+  for (let i = 0; i < this.validateFuncs.length; i++) {
+    let msg = this.validateFuncs[i]();
+    if (msg) {
+      return msg;
+    }
+  }
+}
